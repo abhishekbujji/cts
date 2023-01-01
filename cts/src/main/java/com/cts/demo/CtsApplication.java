9cc55015687e90c652ce76aface932789c4ea8d2
2246560 <2246560@LTIN431052.cts.com>
2023-01-01 22:17:34 +0530

    added comment

--- cts/src/main/java/com/cts/demo/CtsApplication.java
+++ cts/src/main/java/com/cts/demo/CtsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CtsApplication {
 
 	public static void main(String[] args) {
+		//Springboot application
 		SpringApplication.run(CtsApplication.class, args);
 	}
 
