f85f54b264e29ffe44127aad15c5a7a023623784
2246560 <2246560@LTIN431052.cts.com>
2023-01-01 22:22:13 +0530

    added sysout

--- cts/src/main/java/com/cts/demo/CtsApplication.java
+++ cts/src/main/java/com/cts/demo/CtsApplication.java
@@ -9,7 +9,7 @@ public class CtsApplication {
 	public static void main(String[] args) {
 		//Springboot application
 		SpringApplication.run(CtsApplication.class, args);
-		System.out.println();
+		System.out.println("CTS");
 	}
 
 }
